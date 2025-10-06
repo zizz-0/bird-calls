591c005844f716ac2c81abb34464751802e9e26a
zizz-0 <rizzo.zoej@gmail.com>
2025-10-06 14:23:22 -0400

    rm build dir, gitignore

--- Builds/LinuxMakefile/build/intermediate/Debug/include_juce_core_CompilationTime_9257742c.d
+++
@@ -1,3 +0,0 @@
-build/intermediate/Debug/include_juce_core_CompilationTime_9257742c.o: \
- ../../JuceLibraryCode/include_juce_core_CompilationTime.cpp \
- /home/zizz/JUCE/modules/juce_core/juce_core_CompilationTime.cpp
